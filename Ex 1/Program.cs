bd0fb976f7031f9a00cebfbed172bdffe5898d50
MatvienokVS <Skavaava@gmail.com>
2023-01-12 22:14:54 +0300

    Решение ДЗ Семинар 4

---
+++ Ex 1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int.TryParse(ReadLine(), out int num);
+
+int sum = CountDigit(num);
+WriteLine($"{sum}");
+
+
+
+int CountDigit(int num)
+{
+	int result = 0;
+
+	while (Math.Abs(num) > 0)
+	{
+		num /= 10;
+		result += 1;
+	}
+
+	return result;
+}
---
+++ Ex 2/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+Clear();
+
+
+int.TryParse(Readline(), out int num);
+
+int[] array = new array[num];
+
+
